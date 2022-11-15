c861f1fd628b3bf87defeed55f185f1d736c0dfb
nezuko-07 <102100629+nezuko-07@users.noreply.github.com>
2022-11-15 14:58:13 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,84 +1,50 @@
-prediction_1 = ""
-prediction_2 = ""
+      
+Webcam.attach( '#camera' );
 
-Webcam.set({
+camera = document.getElementById("camera");
+      
+  Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
     png_quality:90
   });
 
-camera = document.getElementById("camera");
-
-Webcam.attach('#camera');
-
-      
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 }
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/alSO9a3a7/model.json',modelLoaded);
+  // Inicializa el método de clasificación de imagen con MobileNet
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2ipEBTuSL/model.json',modelLoaded);
 
+  // Cuando se cargue el modelo
   function modelLoaded() {
     console.log('Model Loaded!');
   }
   
-function speak(){
-  var synth = window.speechSynthesis;
-  speak_data_1 = "La primera prediccion es " + prediction_1;
-  speak_data_2 = "Y la segunda prediccion es " + prediction_2;
-  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
-  synth.speak(utterThis);
-}
-
-
+      
   function check()
   {
-    img = document.getElementById('captured_image');
+    img = document.getElementById('selfie_image');
     classifier.classify(img, gotResult);
   }
 
 
+// Función que se ejecuta cuando obtenemos algún error en los resultados
 function gotResult(error, results) {
+  // Muestra el error en la consola
   if (error) {
     console.error(error);
   } else {
+    // Los resultados están en un array ordenado por presición.
     console.log(results);
-    document.getElementById("result_emotion_name").innerHTML = results[0].label;
-    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-    prediction_1 = results[0].label;
-    prediction_2 = results[1].label;
-    speak();
-    if(results[0].label == "feliz")
-    {
-	    document.getElementById("update_emoji").innerHTML = "&#128522;";
-    }
-    if(results[0].label == "triste")
-    {
-	    document.getElementById("update_emoji").innerHTML = "&#128532;";
-    }
-    if(results[0].label == "enojado")
-    {
-	    document.getElementById("update_emoji").innerHTML = "&#128548;";
-    }
-
-    if(results[1].label == "feliz")
-    {
-	    document.getElementById("update_emoji2").innerHTML = "&#128522;";
-    }
-    if(results[1].label == "triste")
-    {
-	    document.getElementById("update_emoji2").innerHTML = "&#128532;";
-    }
-    if(results[1].label == "enojado")
-    {
-	    document.getElementById("update_emoji2").innerHTML = "&#128548;";
-    }
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
   }
 }
-
